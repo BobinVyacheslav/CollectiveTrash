96bca3adfa62942d019ced3ad01dc7e7df6679ad
Bobin_Vyacheslav <bobin.vyacheslav@yandex.ru>
2025-09-03 20:50:52 +0300

    Added Osama class to Java file

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,10 +2,26 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Osama Hitler = new Osama(69, "Adolf");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for (int j = 0; j<n; j++ ) {
             System.out.println("Hello World");
         }
+        Hitler.getAge();
+    }
+}
+class Osama {
+    int age;
+    String name;
+    Osama(int a, String s) {
+        this.age = a;
+        this.name = s;
+    }
+    public void getName() {
+        System.out.println(name);
+    }
+    public void getAge() {
+        System.out.printf("Age of %s is %d", name, age);
     }
 }
